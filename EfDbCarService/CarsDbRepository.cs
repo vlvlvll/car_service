71a10b85e3470ce009c0a4b08cc4c299a6395077
st <stud@10-600-12>
2025-04-25 11:46:51 +0300

    25.04.2025

---
+++ EfDbCarService/CarsDbRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfDbCarService
+{
+    public class CarsDbRepository : ICarsRepository
+    {
+        private readonly CarServiceDbContext dbcontext;
+
+        public CarsDbRepository(CarServiceDbContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        public void Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return dbcontext.Cars.ToList();
+        }
+
+        public Car TryGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EfDbCarService/ICarsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfDbCarService
+{
+    public interface ICarsRepository
+    {
+        void Add(Car car);
+        void Delete(Car car);
+        List<Car> GetAll();
+        Car TryGetById(int id);
+    }
+}
